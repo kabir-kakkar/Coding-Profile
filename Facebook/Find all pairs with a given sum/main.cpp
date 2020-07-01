79574e8d15610cf6d049bc6f27d7a8f0aa613825
kabir-kakkar <48717801+kabir-kakkar@users.noreply.github.com>
2020-07-01 12:03:46 +0530

    Another Approach to "Find all pairs with a given sum"

--- Facebook/Find all pairs with a given sum/main.cpp
+++ Facebook/Find all pairs with a given sum/main.cpp
@@ -5,6 +5,9 @@ int main()
 {
     freopen("input.txt", "r", stdin);
 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
     cin >> t;
 
@@ -12,29 +15,30 @@ int main()
         int n, m, x;
         cin >> n >> m >> x;
 
-        int a[n], b[m];
+        int a[n];
+        map<int, int> s;
 
         for (int i = 0; i < n; i++)
             cin >> a[i];
-        for (int i = 0; i < m; i++)
-            cin >> b[i];
 
         sort(a, a+n);
-        sort(b, b+m);
+
+        for (int i = 0; i < m; i++) {
+            int temp;
+            cin >> temp;
+            s[temp++];
+        }
 
         bool flag = false;
 
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++) {
-                if ((a[i] + b[j]) == x) {
+        for (int i = 0; i < n; i++) {
+            if (s.find(x - a[i]) != s.end()) {
                     if (flag == false) {
-                        cout << a[i] << " " << b[j];
+                        cout << a[i] << " " << x-a[i];
                         flag = true;
                     } else {
-                        cout << ", " << a[i] << " " << b[j];
+                        cout << ", " << a[i] << " " << x-a[i];
                     }
-                }
             }
         }
         if (!flag) {
