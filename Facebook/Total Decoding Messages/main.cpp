4c8e41fd760238f8b206134de244b90217958a38
kabir-kakkar <48717801+kabir-kakkar@users.noreply.github.com>
2020-07-04 17:01:52 +0530

    Written Answer to Total Decoding Ways

--- Facebook/Total Decoding Messages/main.cpp
+++ Facebook/Total Decoding Messages/main.cpp
@@ -1,9 +1,38 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    freopen("input.txt", "r", stdin);
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        int dp[n+1];
+        dp[0] = 1;
+        dp[1] = s[0] == '0' ? 0 : 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            int oneDigit = std::stoi (s.substr (i-1, i));
+            int twoDigit = std::stoi (s.substr (i-2, i));
+
+            if (oneDigit >= 1) {
+                dp[i] += dp[i-1];
+            }
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i] += dp[i-2];
+            }
+        }
+        cout << dp[n] << "\n";
+    }
+
     return 0;
 }
