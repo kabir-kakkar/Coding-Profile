128cebde41107d366c3914ac349f83809e249a64
kabir-kakkar <48717801+kabir-kakkar@users.noreply.github.com>
2020-07-09 17:51:17 +0530

    Updated

--- Facebook/Word Boggle/main.cpp
+++ Facebook/Word Boggle/main.cpp
@@ -1,7 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int n, m, x;
+char boggle[10][10];
+string dictonary[12];
+
+bool isWord(string &str) {
+    for (int i = 0; i < x; i++)
+        if (str.compare(dictonary[i]) == 0)
+            return true;
+    return false;
+}
+
+void findWordsUtil (bool visited[][10], int i, int j, string &str) {
+    visited[i][j] = true;
+    str = str + boggle[i][j];
+
+    if (isWord(str))
+        cout << str << endl;
+
+    for (int row = i-1; row <= i+1 && row<n; row++) {
+        for (int col = j-1; col <= j+1 && col<m; col++) {
+            if (row >= 0 && col >= 0 && !visited[row][col]) {
+                findWordsUtil (visited, i, j, str);
+            }
+        }
+    }
+}
+
+int main() {
+  //  freopen("input.txt", "r", stdin);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int i, j;
+        cin >> x;
+
+
+        for (i = 0; i < x; i++) {
+            cin >> dictonary[i];
+        }
+
+        cin >> n >> m;
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                cin >> boggle[i][j];
+            }
+        }
+
+        bool visited[10][10] = {{false}};
+        string str = "";
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                findWordsUtil (visited, i, j, str);
+            }
+        }
+
+    }
+
     return 0;
 }
