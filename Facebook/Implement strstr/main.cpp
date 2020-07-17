dab1dbfaad77afd911a7bde8cf81adafeaf2379f
kabir <kabir.kakkar4@gmail.com>
2020-07-17 12:46:25 +0530

    Update Implement StrStr

--- Facebook/Implement strstr/main.cpp
+++ Facebook/Implement strstr/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int strstr (string s, string x);
+void computeLPSArray (string x, int m, int* lps);
 
 int main() {
 	// your code goes here
@@ -38,5 +39,54 @@ int strstr (string s, string x) {
 //    return -1;
 
     //SIMPLE APPROACH 2
-    return s.find(x);
+//    return s.find(x);
+
+    //KMP APPROACH
+    int M = x.length();
+    int N = s.length();
+
+    int lps[M];
+
+    computeLPSArray (x, M, lps);
+
+    int i = 0, j = 0;
+    while ( i < N ) {
+        if (x[j] == s[i]) {
+            j++;
+            i++;
+        }
+        if (j == M) {
+            return (i-j);
+            j = lps[j-1];
+        }
+        else if (i < N && x[j] != s[i]) {
+            if (j != 0)
+                j = lps[j-1];
+            else
+                i++;
+        }
+    }
+    return -1;
+}
+
+void computeLPSArray (string x, int m, int* lps) {
+    int len = 0;
+
+    lps[0] = 0;
+
+    int i = 1;
+    while (i < m) {
+        if (x[i] == x[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = lps[len-1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
 }
